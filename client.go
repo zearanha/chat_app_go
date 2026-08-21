fc5b5a4b7347c1729a1334845e75b9b291efa86a
zearanha <JAranha21@gmail.com>
2026-08-21 18:13:47 -0300

    Read and write in separate goroutines

---
+++ client.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+type Client struct {
+	hub *Hub
+	conn *websocket.Conn
+	send chan []byte
+}
+
+func (c *Client) readPump() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure){
+				log.Printf("erro inesperado: %v", err)
+			}
+			break
+		}
+		c.hub.broadcast <- message
+	}
+}
+
+
+func (c *Client) writePump() {
+	ticker := time.NewTicker(54 * time.Second)
+	defer func () {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+
+	for {
+		select {
+		case message, ok := <- c.send:
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+			c.conn.WriteMessage(websocket.TextMessage, message)
+		case <- ticker.C:
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil{
+				return
+			}
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/zearanha/chat_app_go
+
+go 1.26.5
+
+require github.com/gorilla/websocket v1.5.3
